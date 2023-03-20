eaff4984f0324c5e7585314b7aec237b6888c161
Yongheng Chen <changochen1@gmail.com>
2023-03-20 20:49:20 +0000

    Keep cleaning shit.

--- srcs/internal/include/mutate.h
+++ srcs/internal/include/mutate.h
@@ -47,24 +47,12 @@ public:
 
   // string validate(IR * root); // check the sql statement is valid
 
-  void init(string f_testcase = "", string f_common_string = "",
-            string file2d = "", string file1d = "",
-            string f_gen_type = ""); // DONE
-
   void init_convertable_ir_type_map();
-  void init_ir_library(string filename); // DONE
   void init_ir_library_from_a_file(string filename);
-  void init_value_library();                     // DONE
-  void init_common_string(string filename);      // DONE
-  void init_data_library(string filename);       // DONE
-  void init_data_library_2d(string filename);    // DONE
-  void init_not_mutatable_type(string filename); // DONE
-  void init_safe_generate_type(string filename);
   void add_ir_to_library(IR *);         // DONE
   void add_ir_to_library_limited(IR *); // DONE
 
   IR *get_ir_from_library(IRTYPE); // DONE
-  IR *generate_ir_by_type(IRTYPE); // Done
 
   void extract_struct(IR *); // Done
 
@@ -75,44 +63,16 @@ public:
 
   bool can_be_mutated(IR *);
 
-  // private:
-
-  // IR* generate_ir_by_type(IRTYPE type);
-
   static const unsigned max_ir_library_size_ = 0x200;
   IR *record_ = NULL;
   IR *mutated_root_ = NULL;
   map<IRTYPE, vector<IR *>> ir_library_;
   map<IRTYPE, set<unsigned long>> ir_library_hash_;
 
-  vector<string> string_library_;
-  set<unsigned long> string_library_hash_;
-  vector<unsigned long> value_library_;
-
-  map<DATATYPE, map<DATATYPE, RELATIONTYPE>> relationmap_;
-
-  vector<string> common_string_library_;
   set<IRTYPE> not_mutatable_types_;
   set<IRTYPE> string_types_;
   set<IRTYPE> int_types_;
   set<IRTYPE> float_types_;
-
-  set<IRTYPE> safe_generate_type_;
-  set<IRTYPE> split_stmt_types_;
-  set<IRTYPE> split_substmt_types_;
-
-  map<DATATYPE, vector<string>> data_library_;
-  map<DATATYPE, map<string, map<DATATYPE, vector<string>>>> data_library_2d_;
-
-  map<DATATYPE, vector<string>> g_data_library_;
-  map<DATATYPE, set<unsigned long>> g_data_library_hash_;
-  map<DATATYPE, map<string, map<DATATYPE, vector<string>>>> g_data_library_2d_;
-  map<DATATYPE, map<string, map<DATATYPE, vector<string>>>>
-      g_data_library_2d_hash_;
-
-  map<int, map<DATATYPE, vector<IR *>>> scope_library_;
-
-  set<unsigned long> global_hash_;
 
   map<IRTYPE, set<IRTYPE>> m_convertable_map_;
 
@@ -122,6 +82,4 @@ public:
 
 unsigned int calc_node(IR *root);
 bool contain_fixme(IR *);
-void remove_definition(IR *&);
-void extract_struct_after_mutation(IR *);
 #endif
--- srcs/internal/srcs/mutate.cpp
+++ srcs/internal/srcs/mutate.cpp
@@ -62,25 +62,6 @@ IR *Mutator::deep_copy_with_record(const IR *root, const IR *record) {
   return copy_res;
 }
 
-// Need No fix
-/*
-bool Mutator::check_node_num(IR * root, unsigned int limit) const{
-
-    auto v_statements = extract_statement(root);
-    bool is_good = true;
-
-    for(auto stmt: v_statements){
-        if(calc_node(stmt) > limit){
-            is_good = false;
-            break;
-        }
-    }
-
-    return is_good;
-}
-*/
-
-// Need Minor Fix
 vector<IR *> Mutator::mutate_all(vector<IR *> &v_ir_collector) {
   vector<IR *> res;
   set<unsigned long> res_hash;
@@ -92,32 +73,19 @@ vector<IR *> Mutator::mutate_all(vector<IR *> &v_ir_collector) {
   for (auto ir : v_ir_collector) {
     if (ir == root)
       continue;
-// cout << "Checking: " << ir->to_string() << endl;
-#ifdef MUTATE_UNKNOWN
-    if (not_mutatable_types_.find(ir->type_) != not_mutatable_types_.end() ||
-        !can_be_mutated(ir))
-      continue;
-#else
     if (ir->type_ == kUnknown ||
         not_mutatable_types_.find(ir->type_) != not_mutatable_types_.end() ||
         is_leaf(ir) || !can_be_mutated(ir))
       continue;
-#endif
 
-    // cout << "Yes" << endl;
     if (MUTATE_DBG)
       cout << "Mutating type: " << get_string_by_nodetype(ir->type_) << endl;
     vector<IR *> v_mutated_ir = mutate(ir);
-    // cout << "this size: " << v_mutated_ir.size() << endl;
 
     for (auto i : v_mutated_ir) {
       IR *new_ir_tree = deep_copy_with_record(root, ir);
       if (MUTATE_DBG)
         cout << "NEW type: " << get_string_by_nodetype(i->type_) << endl;
-
-#ifndef SYNTAX_ONLY
-// remove_definition(i);
-#endif
 
       replace(new_ir_tree, this->record_, i);
 
@@ -148,12 +116,7 @@ vector<IR *> Mutator::mutate_all(vector<IR *> &v_ir_collector) {
 void Mutator::add_ir_to_library(IR *cur) {
   extract_struct(cur);
 
-  // #ifdef SYNTAX_ONLY
   add_ir_to_library_limited(cur);
-  // #else
-  // cur = deep_copy(cur);
-  // add_ir_to_library_no_deepcopy(cur);
-  // #endif
   return;
 }
 
@@ -191,118 +154,6 @@ void Mutator::init_convertable_ir_type_map() {
   }
 }
 
-void Mutator::init_common_string(string filename) {
-  common_string_library_.push_back("DO_NOT_BE_EMPTY");
-  if (filename != "") {
-    ifstream input_string(filename);
-    string s;
-
-    while (getline(input_string, s)) {
-      common_string_library_.push_back(s);
-    }
-  }
-}
-
-void Mutator::init_data_library_2d(string filename) {
-  ifstream input_file(filename);
-  string s;
-
-  cout << "[*] init data_library_2d: " << filename << endl;
-  while (getline(input_file, s)) {
-    vector<string> v_strbuf;
-    auto prev_pos = -1;
-    for (int i = 0; i < 3; i++) {
-      auto pos = s.find(" ", prev_pos + 1);
-      v_strbuf.push_back(s.substr(prev_pos + 1, pos - prev_pos - 1));
-      prev_pos = pos;
-    }
-    v_strbuf.push_back(s.substr(prev_pos + 1, s.size() - prev_pos - 1));
-
-    auto data_type1 = get_datatype_by_string(v_strbuf[0]);
-    auto data_type2 = get_datatype_by_string(v_strbuf[2]);
-    // cout << "g_data_library_2d_[" << data_type1 << "][" << v_strbuf[1] <<
-    // "][" << data_type2 <<"].push_back(" << v_strbuf[3] <<")" << endl;
-    g_data_library_2d_[data_type1][v_strbuf[1]][data_type2].push_back(
-        v_strbuf[3]);
-  }
-
-  return;
-}
-
-void Mutator::init_data_library(string filename) {
-  ifstream input_file(filename);
-  string s;
-
-  cout << "[*] init data_library: " << filename << endl;
-  while (getline(input_file, s)) {
-    auto pos = s.find(" ");
-    if (pos == string::npos)
-      continue;
-    auto data_type = get_datatype_by_string(s.substr(0, pos));
-    auto v = s.substr(pos + 1, s.size() - pos - 1);
-    g_data_library_[data_type].push_back(v);
-  }
-
-  return;
-}
-
-void Mutator::init_value_library() {
-  vector<unsigned long> value_lib_init = {0,
-                                          (unsigned long)LONG_MAX,
-                                          (unsigned long)ULONG_MAX,
-                                          (unsigned long)CHAR_BIT,
-                                          (unsigned long)SCHAR_MIN,
-                                          (unsigned long)SCHAR_MAX,
-                                          (unsigned long)UCHAR_MAX,
-                                          (unsigned long)CHAR_MIN,
-                                          (unsigned long)CHAR_MAX,
-                                          (unsigned long)MB_LEN_MAX,
-                                          (unsigned long)SHRT_MIN,
-                                          (unsigned long)INT_MIN,
-                                          (unsigned long)INT_MAX,
-                                          (unsigned long)SCHAR_MIN,
-                                          (unsigned long)SCHAR_MIN,
-                                          (unsigned long)UINT_MAX,
-                                          (unsigned long)FLT_MAX,
-                                          (unsigned long)DBL_MAX,
-                                          (unsigned long)LDBL_MAX,
-                                          (unsigned long)FLT_MIN,
-                                          (unsigned long)DBL_MIN,
-                                          (unsigned long)LDBL_MIN};
-
-  value_library_.insert(value_library_.begin(), value_lib_init.begin(),
-                        value_lib_init.end());
-
-  return;
-}
-
-void Mutator::init_ir_library(string filename) {
-  ifstream input_file(filename);
-  string line;
-
-  cout << "[*] init ir_library: " << filename << endl;
-  while (getline(input_file, line)) {
-    if (line.empty())
-      continue;
-    // cout << "parsing " << line << endl;
-    auto p = parser(line);
-    if (p == NULL)
-      continue;
-
-    vector<IR *> v_ir;
-    auto res = p->translate(v_ir);
-    p->deep_delete();
-    p = NULL;
-
-    // string strip_sql = extract_struct(res);
-    // extract_struct(res);
-
-    add_ir_to_library(res);
-    deep_delete(res);
-  }
-  return;
-}
-
 void Mutator::init_ir_library_from_a_file(string filename) {
   char content[0x4000] = {0};
   auto fd = open(filename.c_str(), 0);
@@ -327,78 +178,6 @@ void Mutator::init_ir_library_from_a_file(string filename) {
   return;
 }
 
-void Mutator::init_safe_generate_type(string filename) {
-  ifstream input_file(filename);
-  string line;
-
-  cout << "[*] init safe generate type: " << filename << endl;
-  while (getline(input_file, line)) {
-    if (line.empty())
-      continue;
-    auto node_type = get_nodetype_by_string("k" + line);
-    safe_generate_type_.insert(node_type);
-  }
-}
-
-// Need Minor fix
-void Mutator::init(string f_testcase, string f_common_string, string file2d,
-                   string file1d, string f_gen_type) {
-
-  // init lib from multiple sql
-  if (!f_testcase.empty())
-    init_ir_library(f_testcase);
-
-  // Keep
-  // init value_library_
-  init_value_library();
-
-  // init common_string_library
-  if (!f_common_string.empty())
-    init_common_string(f_common_string);
-
-  // Fix Here
-  // init data_library_2d
-  if (!file2d.empty())
-    init_data_library_2d(file2d); // NEEDFIX
-
-  if (!file1d.empty())
-    init_data_library(file1d);
-  if (!f_gen_type.empty())
-    init_safe_generate_type(f_gen_type);
-  // Fix here
-  /*
-  relationmap[id_column_name] = id_top_table_name;
-  relationmap[id_table_name] = id_top_table_name;
-  relationmap[id_create_column_name] = id_create_table_name;
-  relationmap[id_pragma_value] = id_pragma_name;
-  cross_map[id_top_table_name] = id_create_table_name;
-  */
-  float_types_.insert({kFloatLiteral});
-  int_types_.insert(kIntLiteral);
-  string_types_.insert(kStringLiteral);
-
-  /*
-  relationmap_[kDataColumnName][kDataTableName] = kRelationSubtype;
-  relationmap_[kDataPragmaValue][kDataPragmaKey] = kRelationSubtype;
-  relationmap_[kDataTableName][kDataTableName] = kRelationElement;
-  relationmap_[kDataColumnName][kDataColumnName] = kRelationElement;
-  */
-
-  // split_stmt_types_.insert(kStmt);
-  // split_substmt_types_.insert({kStmt, kSelectClause, kSelectStmt});
-
-  // not_mutatable_types_.insert({kProgram, kStmtlist, kStmt, kCreateStmt,
-  // kDropStmt, kCreateTableStmt, kCreateIndexStmt, kCreateTriggerStmt,
-  // kCreateViewStmt, kCreateFunctionStmt, kDropIndexStmt, kDropTableStmt,
-  // kDropTriggerStmt, kDropViewStmt, kSelectStmt, kUpdateStmt, kInsertStmt,
-  // kAlterStmt, kReindexStmt}); not_mutatable_types_.insert({kProgram});
-
-  init_convertable_ir_type_map();
-
-  return;
-}
-
-// Need No Fix
 vector<IR *> Mutator::mutate(IR *input) {
   vector<IR *> res;
 
@@ -736,16 +515,6 @@ bool Mutator::lucky_enough_to_be_mutated(unsigned int mutated_times) {
   return false;
 }
 
-IR *Mutator::generate_ir_by_type(IRTYPE type) {
-  auto ast_node = generate_ast_node_by_type(type);
-  ast_node->generate();
-  vector<IR *> tmp_vector;
-  ast_node->translate(tmp_vector);
-  // ast_node->deep_delete();
-  assert(tmp_vector.size());
-  return tmp_vector[tmp_vector.size() - 1];
-}
-
 // Fix, if no item, generate from scratch
 IR *Mutator::get_ir_from_library(IRTYPE type) {
 
@@ -793,42 +562,6 @@ void Mutator::debug(IR *root) {
   }
 }
 
-void extract_struct_after_mutation(IR *root) {
-
-  if (root->left_) {
-    if (root->left_->data_type_ == kDataFixUnit) {
-      if (contain_fixme(root->left_)) {
-        // auto save_ir_type = root->left_->type_;
-        auto save_ir_id = root->left_->id_;
-        auto save_scope = root->left_->scope_id_;
-        deep_delete(root->left_);
-        root->left_ = new IR(kStringLiteral, "FIXME");
-        // root->left_->type_ = save_ir_type;
-        root->left_->scope_id_ = save_scope;
-        root->left_->id_ = save_ir_id;
-      }
-    } else {
-      extract_struct_after_mutation(root->left_);
-    }
-  }
-  if (root->right_) {
-    if (root->right_->data_type_ == kDataFixUnit) {
-      if (contain_fixme(root->right_)) {
-        auto save_ir_id = root->right_->id_;
-        // auto save_ir_type = root->right_->type_;
-        auto save_scope = root->right_->scope_id_;
-        deep_delete(root->right_);
-        root->right_ = new IR(kStringLiteral, "FIXME");
-        // root->right_->type_ = save_ir_type;
-        root->right_->scope_id_ = save_scope;
-        root->right_->id_ = save_ir_id;
-      }
-    } else {
-      extract_struct_after_mutation(root->right_);
-    }
-  }
-  return;
-}
 void Mutator::extract_struct(IR *root) {
   static unsigned long iid = 0;
   auto type = root->type_;
@@ -885,13 +618,6 @@ void Mutator::extract_struct(IR *root) {
   }
 }
 
-/*
-void Mutator::reset_data_library() {
-  data_library_.clear();
-  data_library_2d_.clear();
-}
-*/
-
 unsigned int calc_node(IR *root) {
   unsigned int res = 0;
   if (root->left_)
@@ -900,99 +626,6 @@ unsigned int calc_node(IR *root) {
     res += calc_node(root->right_);
 
   return res + 1;
-}
-
-/*
-vector<IR *> Mutator::split_to_stmt(IR *root, map<IR **, IR *> &m_save,
-                                    set<IRTYPE> &split_set) {
-  vector<IR *> res;
-  deque<IR *> bfs = {root};
-
-  while (!bfs.empty()) {
-    auto node = bfs.front();
-    bfs.pop_front();
-
-    if (node && node->left_)
-      bfs.push_back(node->left_);
-    if (node && node->right_)
-      bfs.push_back(node->right_);
-
-    if (node->left_ && find(split_set.begin(), split_set.end(),
-                            node->left_->type_) != split_set.end()) {
-      res.push_back(node->left_);
-      m_save[&node->left_] = node->left_;
-      node->left_ = NULL;
-    }
-    if (node->right_ && find(split_set.begin(), split_set.end(),
-                             node->right_->type_) != split_set.end()) {
-      res.push_back(node->right_);
-      m_save[&node->right_] = node->right_;
-      node->right_ = NULL;
-    }
-  }
-
-  if (find(split_set.begin(), split_set.end(), root->type_) != split_set.end())
-    res.push_back(root);
-
-  // reverse(res.begin(), res.end());
-  return res;
-}
-
-bool Mutator::connect_back(map<IR **, IR *> &m_save) {
-  for (auto &iter : m_save) {
-    *(iter.first) = iter.second;
-  }
-  return true;
-}
-*/
-
-/*
-static set<IR *> visited;
-
-bool Mutator::fix_one(IR *stmt_root,
-                      map<int, map<DATATYPE, vector<IR *>>> &scope_library) {
-  visited.clear();
-  analyze_scope(stmt_root);
-  auto graph = build_graph(stmt_root, scope_library);
-
-#ifdef GRAPHLOG
-  for (auto &iter : graph) {
-    cout << "Node: " << iter.first->to_string() << " connected with:" << endl;
-    for (auto &k : iter.second) {
-      cout << k->to_string() << endl;
-    }
-    cout << "--------" << endl;
-  }
-  cout << "OUTPUT END" << endl;
-#endif
-  // exit(0);
-  return fill_stmt_graph(graph);
-}
-*/
-
-#define has_element(a, b) (find(a.begin(), a.end(), b) != (a).end())
-#define has_key(a, b) ((a).find(b) != (a).end())
-
-static IR *search_mapped_ir(IR *ir, DATATYPE type) {
-  vector<IR *> to_search;
-  vector<IR *> backup;
-  to_search.push_back(ir);
-  while (!to_search.empty()) {
-    for (auto i : to_search) {
-      if (i->data_type_ == type) {
-        return i;
-      }
-      if (i->left_) {
-        backup.push_back(i->left_);
-      }
-      if (i->right_) {
-        backup.push_back(i->right_);
-      }
-    }
-    to_search = move(backup);
-    backup.clear();
-  }
-  return NULL;
 }
 
 bool Mutator::can_be_mutated(IR *cur) {
@@ -1029,30 +662,4 @@ bool contain_fixme(IR *ir) {
   }
 
   return false;
-}
-
-void remove_definition(IR *&ir) {
-
-  if (ir->data_type_ == kDataVarDefine) {
-    deep_delete(ir);
-    ir = NULL;
-    return;
-  }
-
-  if (ir->left_) {
-    if (ir->left_->data_type_ == kDataVarDefine) {
-      deep_delete(ir->left_);
-      ir->left_ = NULL;
-    } else {
-      remove_definition(ir->left_);
-    }
-  }
-  if (ir->right_) {
-    if (ir->right_->data_type_ == kDataVarDefine) {
-      deep_delete(ir->right_);
-      ir->right_ = NULL;
-    } else {
-      remove_definition(ir->right_);
-    }
-  }
 }
