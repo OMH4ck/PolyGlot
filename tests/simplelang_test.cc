6d64206e585c06e96c237e3158e56e4f44968f38
Yongheng Chen <changochen1@gmail.com>
2023-04-02 02:07:35 +0000

    X

--- tests/simplelang_test.cc
+++ tests/simplelang_test.cc
@@ -1,8 +1,10 @@
 #include <gtest/gtest.h>
 
 #include <string_view>
+#include <unordered_set>
 
 #include "config_misc.h"
+#include "ir.h"
 #include "mutate.h"
 #include "utils.h"
 
@@ -36,6 +38,9 @@ INSTANTIATE_TEST_SUITE_P(ValidTestCase, ParserTest,
     int a = 1;
   )V0G0N",
                              R"V0G0N(
+    int a = 'x';
+  )V0G0N",
+                             R"V0G0N(
   STRUCT c {
   int a;
   int b;
@@ -55,8 +60,6 @@ TEST(MutatorTest, MutateInitGoodTestCasesOnly) {
 
   polyglot::mutation::Mutator mutator;
 
-  vector<IR*> ir_set;
-
   std::string init_file_path = polyglot::gen::GetInitDirPath();
   vector<string> file_list = get_all_files_in_dir(init_file_path.c_str());
 
@@ -70,4 +73,60 @@ TEST(MutatorTest, MutateInitGoodTestCasesOnly) {
 
   // Put an bad test case in the init dir.
   ASSERT_EQ(valid_test_case_count, file_list.size() - 1);
+}
+
+class MutatorTestF : public testing::Test {
+ protected:
+  void SetUp() override {
+    std::string init_file_path = polyglot::gen::GetInitDirPath();
+    vector<string> file_list = get_all_files_in_dir(init_file_path.c_str());
+    for (auto& f : file_list) {
+      mutator.init_ir_library_from_a_file(f);
+    }
+  }
+
+  polyglot::mutation::Mutator mutator;
+};
+
+TEST_F(MutatorTestF, MutateGenerateDifferentTestCases) {
+  std::string_view test_case = "int a = 1;";
+
+  std::unordered_set<std::string> unique_test_cases;
+
+  while (unique_test_cases.size() < 20) {
+    // Avoid mutated_times_ too large, so we make it clean every time.
+    vector<IR*> ir_set;
+    Program* program_root = parser(test_case.data());
+    auto root = program_root->translate(ir_set);
+    program_root->deep_delete();
+
+    auto mutated_irs = mutator.mutate_all(ir_set);
+    for (auto& ir : mutated_irs) {
+      unique_test_cases.insert(ir->to_string());
+      deep_delete(ir);
+    }
+    deep_delete(root);
+  }
+
+  ASSERT_GE(unique_test_cases.size(), 20);
+}
+
+TEST_F(MutatorTestF, MutateGenerateParsableTestCases) {
+  std::string_view test_case = "int a = 1;";
+
+  for (size_t i = 0; i < 1; ++i) {
+    vector<IR*> ir_set;
+    Program* program_root = parser(test_case.data());
+    auto root = program_root->translate(ir_set);
+    program_root->deep_delete();
+
+    auto mutated_irs = mutator.mutate_all(ir_set);
+    for (auto& ir : mutated_irs) {
+      Program* new_root = parser(ir->to_string());
+      ASSERT_TRUE(new_root != nullptr) << ir->to_string();
+      new_root->deep_delete();
+      deep_delete(ir);
+    }
+    deep_delete(root);
+  }
 }
