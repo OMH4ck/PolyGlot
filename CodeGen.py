6b43484f6910b8b6d51bf0404714f936c5ebc7eb
Yongheng Chen <changochen1@gmail.com>
2023-04-02 00:43:34 +0000

    X

--- CodeGen.py
+++ CodeGen.py
@@ -1,4 +1,4 @@
-import fire 
+import fire
 import os
 import subprocess
 
@@ -17,11 +17,20 @@ def generate_template_code(grammar_dir):
     subprocess.run("./replace.sh", check=True, shell=True)
 
     os.chdir(cur_dir)
-    subprocess.run(f"python3 Generator.py -i {grammar_dir}/replaced_grammar -t {grammar_dir}/tokens -e {grammar_dir}/extra_flex_rule -s {grammar_dir}/semantic.json", shell=True, check=True)
+    subprocess.run(
+        f"python3 Generator.py -i {grammar_dir}/replaced_grammar -t {grammar_dir}/tokens -e {grammar_dir}/extra_flex_rule -s {grammar_dir}/semantic.json",
+        shell=True,
+        check=True,
+    )
 
     os.chdir("gen/parser")
     subprocess.run("flex flex.l", shell=True, check=True)
-    subprocess.run("bison bison.y --output=bison_parser.cpp --defines=bison_parser.h --verbose -Wconflicts-rr", shell=True, check=True)
+    subprocess.run(
+        "bison bison.y --output=bison_parser.cpp --defines=bison_parser.h --verbose -Wconflicts-rr",
+        shell=True,
+        check=True,
+    )
+
 
 if __name__ == "__main__":
     fire.Fire(generate_template_code)
