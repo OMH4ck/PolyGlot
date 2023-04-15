96f7e1cf29956b5601bc0d665944c7f6d8c9c43d
Yongheng Chen <changochen1@gmail.com>
2023-04-14 20:22:11 -0400

    remove useless files

--- build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-sudo apt update
-sudo apt install -y make g++ bison flex clang-format
--- diff.sh
+++
@@ -1,4 +0,0 @@
-set -e
-diff parser test_data/parser
-diff include test_data/include
-diff src test_data/src
